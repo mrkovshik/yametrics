a94d05cbc55dc3fd24cbd0537f056aa0930fb291
n.kulakov <n.kulakov@ipc.kg>
2024-03-28 07:53:03 +0600

    updated agent tests

--- cmd/agent/agent_test.go
+++ cmd/agent/agent_test.go
@@ -21,15 +21,19 @@ func Test_getMetrics(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			src.PollMetrics(strg)
-			valPollCount := strg.LoadMetric("PollCount")
-			assert.Equal(t, valPollCount, "1")
-			valAlloc := strg.LoadMetric("Alloc")
-			assert.Equal(t, valAlloc, "1.00")
-			valBuckHashSys := strg.LoadMetric("BuckHashSys")
-			assert.Equal(t, valBuckHashSys, "2.00")
+			valPollCount, err1 := strg.LoadCounter()
+			assert.NoError(t, err1)
+			assert.Equal(t, int64(1), valPollCount)
+			valAlloc, err2 := strg.LoadMetric("Alloc")
+			assert.NoError(t, err2)
+			assert.Equal(t, 1.00, valAlloc)
+			valBuckHashSys, err3 := strg.LoadMetric("BuckHashSys")
+			assert.NoError(t, err3)
+			assert.Equal(t, 2.00, valBuckHashSys)
 			src.PollMetrics(strg)
-			valPollCount = strg.LoadMetric("PollCount")
-			assert.Equal(t, valPollCount, "2")
+			valPollCount, err1 = strg.LoadCounter()
+			assert.NoError(t, err1)
+			assert.Equal(t, int64(2), valPollCount)
 		})
 	}
 }
