8bcaf41a71771b3be4a96b7e65b06ae0f4a20dba
John Doe <kovshik@inbox.ru>
2024-03-31 12:12:53 +0600

    fixed compressing

--- internal/service/agent/request.go
+++ internal/service/agent/request.go
@@ -15,51 +15,56 @@ type RequestBuilder struct {
 }
 
 func NewRequestBuilder() *RequestBuilder {
-	return &RequestBuilder{http.Request{}, nil}
+	req, err := http.NewRequest(http.MethodGet, "", nil)
+	return &RequestBuilder{*req, err}
 }
 
-func (rb RequestBuilder) WithHeaders(headers map[string]string) *RequestBuilder {
-	if rb.err == nil {
-		for header, value := range headers {
-			rb.r.Header.Add(header, value)
-		}
-	}
-	return &rb
+func (rb *RequestBuilder) WithHeader(key, value string) *RequestBuilder {
+	rb.r.Header.Add(key, value)
+	return rb
 }
 
-func (rb RequestBuilder) SetMethod(method string) *RequestBuilder {
+func (rb *RequestBuilder) SetMethod(method string) *RequestBuilder {
 	if rb.err == nil {
 		rb.r.Method = method
 	}
-	return &rb
+	return rb
 }
 
-func (rb RequestBuilder) SetURL(rawUrl string) *RequestBuilder {
+func (rb *RequestBuilder) SetURL(rawUrl string) *RequestBuilder {
 	if rb.err == nil {
 		rb.r.URL, rb.err = url.Parse(rawUrl)
 	}
-	return &rb
+	return rb
 }
 
-func (rb RequestBuilder) AddJSONBody(data any) *RequestBuilder {
+func (rb *RequestBuilder) AddJSONBody(data any) *RequestBuilder {
 	if rb.err == nil {
 		buf := bytes.Buffer{}
 		rb.err = json.NewEncoder(&buf).Encode(data)
 		if rb.err == nil {
 			rb.r.Body = io.NopCloser(&buf)
-			rb.WithHeaders(map[string]string{"Content-Type": "application/json"})
+			rb.WithHeader("Content-Type", "application/json")
 		}
 	}
-	return &rb
+	return rb
 }
 
-func (rb RequestBuilder) Compress() *RequestBuilder {
+func (rb *RequestBuilder) Compress() *RequestBuilder {
 	if rb.err == nil {
-		compressedBody, err := gzip.NewReader(rb.r.Body)
-		if err == nil {
-			rb.r.Body = compressedBody
-			rb.WithHeaders(map[string]string{"Content-Encoding": "gzip"})
+		var compressedBody bytes.Buffer
+		gzipWriter := gzip.NewWriter(&compressedBody)
+		_, err := io.Copy(gzipWriter, rb.r.Body)
+		if err != nil {
+			return &RequestBuilder{err: err}
 		}
+		err = gzipWriter.Close()
+		if err != nil {
+			return &RequestBuilder{err: err}
+		}
+		rb.r.Body = io.NopCloser(&compressedBody)
+		rb.r.ContentLength = int64(compressedBody.Len())
+		rb.WithHeader("Content-Encoding", "gzip")
 	}
-	return &rb
+	return rb
 }
