b7fda9c80da882307fd5c0840d2c4bb2da258a86
n.kulakov <n.kulakov@ipc.kg>
2024-04-05 09:00:27 +0600

    removed group from storage

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -2,12 +2,10 @@ package storage
 
 import "github.com/mrkovshik/yametrics/internal/model"
 
-type (
-	Storage interface {
-		UpdateMetricValue(newMetrics model.Metrics)
-		GetMetricByModel(newMetrics model.Metrics) (model.Metrics, error)
-		GetAllMetrics() (string, error)
-		StoreMetrics(path string) error
-		RestoreMetrics(path string) error
-	}
-)
+type Storage interface {
+	UpdateMetricValue(newMetrics model.Metrics)
+	GetMetricByModel(newMetrics model.Metrics) (model.Metrics, error)
+	GetAllMetrics() (string, error)
+	StoreMetrics(path string) error
+	RestoreMetrics(path string) error
+}
