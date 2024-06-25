e832bc909de79ade570aed4eaf9ebe2a3a4aae5c
N.Kulakov <nikolay-kulakov@inbox.ru>
2024-06-25 10:54:35 +0600

    added some examples

--- api/rest/rest_api_test.go
+++ api/rest/rest_api_test.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"crypto/hmac"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"strconv"
@@ -327,4 +328,59 @@ func Test_server(t *testing.T) {
 			}
 		})
 	}
+}
+
+func Example() {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		logger.Fatal("zap.NewDevelopment",
+			zap.Error(err))
+	}
+	defer logger.Sync()
+	sugar := logger.Sugar()
+
+	cfg, err := config.GetConfigs()
+	if err != nil {
+		sugar.Fatal("cfg.GetConfigs", err)
+	}
+	ctx := context.Background()
+	metricStorage := storage.NewMapStorage()
+	metricService := service.NewMetricService(metricStorage, &cfg, sugar)
+	gauge := 2.5
+	counter := int64(2)
+	metric1 := model.Metrics{
+		ID:    "test_gauge",
+		MType: model.MetricTypeGauge,
+		Value: &gauge,
+	}
+	metric2 := model.Metrics{
+		ID:    "test_counter",
+		MType: model.MetricTypeCounter,
+		Delta: &counter,
+	}
+	if err := metricService.UpdateMetrics(ctx, []model.Metrics{
+		metric1,
+		metric2,
+	}); err != nil {
+		sugar.Fatal("metricService.UpdateMetrics", err)
+	}
+
+	m1, err := metricService.GetMetric(ctx, model.Metrics{
+		ID:    "test_counter",
+		MType: model.MetricTypeCounter,
+	})
+
+	m2, err := metricService.GetMetric(ctx, model.Metrics{
+		ID:    "test_gauge",
+		MType: model.MetricTypeGauge,
+	})
+
+	if err != nil {
+		sugar.Fatal("metricService.UpdateMetrics", err)
+	}
+	fmt.Println(*m1.Delta, m1.ID, m1.MType)
+	fmt.Println(*m2.Value, m2.ID, m2.MType)
+	// Output:
+	// 2 test_counter counter
+	// 2.5 test_gauge gauge
 }
--- cmd/agent/agent.go
+++ cmd/agent/agent.go
@@ -14,27 +14,33 @@ import (
 )
 
 func main() {
-	var (
-		strg = storage.NewMapStorage()
-		src  = metrics.NewRuntimeMetrics()
-	)
+	// Initialize storage and metrics source
+	strg := storage.NewMapStorage()
+	src := metrics.NewRuntimeMetrics()
 
+	// Initialize logging with zap
 	logger, err := zap.NewDevelopment()
 	if err != nil {
-		log.Fatal("zap.NewDevelopment",
-			zap.Error(err))
+		log.Fatal("zap.NewDevelopment", zap.Error(err))
 	}
-	cfg, err := config.GetConfigs()
-	if err != nil {
-		logger.Fatal("config.GetConfigs",
-			zap.Error(err))
-	}
+
+	// Flushes buffered log entries before program exits
 	defer logger.Sync() //nolint:all
 	sugar := logger.Sugar()
-	ctx := context.Background()
+
+	// Get configuration settings
+	cfg, err := config.GetConfigs()
+	if err != nil {
+		logger.Fatal("config.GetConfigs", zap.Error(err))
+	}
+
+	// Create agent instance with dependencies
 	agent := service.NewAgent(src, cfg, strg, sugar)
+
+	// Log agent configuration
 	sugar.Infof("Running agent on %v\npoll interval = %v\nreport interval = %v\n", cfg.Address, cfg.PollInterval, cfg.ReportInterval)
 
+	// Create tickers for polling and sending metrics
 	pollTicker := time.NewTicker(time.Duration(cfg.PollInterval) * time.Second)
 	defer pollTicker.Stop()
 	pollUtilTicker := time.NewTicker(time.Duration(cfg.PollInterval) * time.Second)
@@ -42,8 +48,11 @@ func main() {
 	sendTicker := time.NewTicker(time.Duration(cfg.ReportInterval) * time.Second)
 	defer sendTicker.Stop()
 
+	// Start goroutines for polling and sending metrics
 	go agent.PollMetrics(pollTicker.C)
 	go agent.PollUitlMetrics(pollUtilTicker.C)
-	go agent.SendMetrics(ctx, sendTicker.C)
+	go agent.SendMetrics(context.Background(), sendTicker.C)
+
+	// Block indefinitely to keep the agent running
 	select {}
 }
